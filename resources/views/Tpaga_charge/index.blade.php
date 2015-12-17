e2d7e34e6297433793688fc3cbf6ce3c0220be15
smanriquen <smanriquen@gmail.com>
2015-12-17 18:17:26 -0500

    Working site

--- resources/views/Tpaga_charge/index.blade.php
+++ resources/views/Tpaga_charge/index.blade.php
@@ -3,7 +3,8 @@
 @section('header')
 
   <script src="{{asset('assets/js/jquery.payment.js')}}"></script>
-  
+ 
+
   <style type="text/css" media="screen">
     .has-error input {
       border-width: 2px;
@@ -11,44 +12,82 @@
     }
 
     .validation.text-danger:after {
-      content: 'Validation failed';
+      content: 'Invalid data';
     }
 
     .validation.text-success:after {
       content: 'Validation passed';
     }
+
+
+    .panel-default > .panel-heading-custom {
+        background: #3399FF; color: #fff; font-size: 25px;
+        height: 50px; text-align: center; 
+    }
+
+    .padding{
+      padding: 10px;
+    }
+    .center{
+      text-align: center;
+    }
   </style>
 
-
-
-<script>
+  <script>
     jQuery(function($) {
       $('.cardnumber').payment('formatCardNumber');
       $('.securitycode').payment('formatCardCVC');
+      $('.duedate').payment('formatCardExpiry');
       $.fn.toggleInputError = function(erred) {
         this.parent('.form-group').toggleClass('has-error', erred);
         return this;
       };
+
       $('form').submit(function(e) {
-        e.preventDefault();
-        var cardType = $.payment.cardType($('.cardnumber').val());
-        //$('.validation').html(date);
-        $('.cardnumber').toggleInputError(!$.payment.validateCardNumber($('.cardnumber').val()));
-        $('.securitycode').toggleInputError(!$.payment.validateCardCVC($('.securitycode').val(), cardType));
-        $('.cardbrand').text(cardType);
-        $('.validation').removeClass('text-danger text-success');
-        $('.validation').addClass($('.has-error').length ? 'text-danger' : 'text-success');
+
+      e.preventDefault();
+
+      var cardType = $.payment.cardType($('.cardnumber').val());
+      //$('.validation').html(date);
+      $('.cardnumber').toggleInputError(!$.payment.validateCardNumber($('.cardnumber').val()));
+      $('.securitycode').toggleInputError(!$.payment.validateCardCVC($('.securitycode').val(), cardType));
+      $('.duedate').toggleInputError(!$.payment.validateCardExpiry($('.duedate').payment('cardExpiryVal')));
+
+      $('.cardbrand').text(cardType);
+      $('.validation').removeClass('text-danger text-success');
+      $('.validation').addClass($('.has-error').length ? 'text-danger' : 'text-success');
+      
+      var errors = $('.has-error');
+
+      if (!errors.length)
+      {
+
+        $(this).unbind('submit').submit();
+        $('.alert').removeClass('alert-danger');
+        $('.alert').addClass('alert-success');
+      }
+      else
+      {
+        
+        $('.validation').show();
+      }
+
       });
     });
   </script>
+
   
 @stop
 
 
-@section('content')
+@section('maincontent')
+
+   
 
 
-    <div class="container">
+    <h1 class = "center"> SELECT YOUR PRODUCT!</h1>
+
+    <div class="container padding">
       {!! Form::open(array('url' => '/charge', 'method' => 'post', 'autocomplete' =>'on')) !!}
       <div class="row">
         <div class="col-xs-4 ">
@@ -84,26 +123,36 @@
       </div>
     </div>
 
+
+@stop
+@section('paymentcontent')    
+
     <div class="container">
+      
+      
       <div class="panel panel-default">
-        <div class="panel-heading">CREDIT CARD DETAILS</label></div>
+        <div class="panel-heading panel-heading-custom">CREDIT CARD DETAILS</label></div>
         <div class="panel-body ">
           
 
               <!-- Title form input -->
               
             <div class="row">
-              <div class="col-xs-6">
-                <div class="form-group">
-                  {!! Form::label('cardnumber', 'Card number:') !!}
-                  {!! Form::text('cardnumber', null,  ['class' => 'form-control cardnumber', 'required' => 'required', 'placeholder' =>'•••• •••• •••• ••••', 'autocomplete' => 'cardnumber']) !!}
-                  <span class="cardbrand"></span>
+              <div class="col-xs-6"> 
+                <div class='form-field'> 
+                  <div class="form-group">
+                    {!! Form::label('cardnumber', 'Card number:') !!}
+                    {!! Form::text('cardnumber', null,  ['class' => 'form-control cardnumber', 'required' => 'required', 'placeholder' =>'•••• •••• •••• ••••', 'autocomplete' => 'cardnumber']) !!}
+                    <span class="cardbrand"></span>
+                  </div>
                 </div>
               </div>
-              <div class="col-xs-6">  
-                <div class="form-group">
-                  {!! Form::label('installments', 'Installments:') !!}
-                  {!! Form::text('installments', null, ['class' => 'form-control installments', 'maxlength' => 2, 'required' => 'required', 'autocomplete' => 'installments']) !!}
+              <div class="col-xs-6"> 
+                <div class='form-field'>  
+                  <div class="form-group">
+                    {!! Form::label('installments', 'Installments:') !!}
+                    {!! Form::text('installments', null, ['class' => 'form-control installments', 'maxlength' => 2, 'required' => 'required', 'autocomplete' => 'installments']) !!}
+                  </div>
                 </div>
               </div>
             </div>
@@ -113,15 +162,19 @@
             
             <div class="row">
               <div class="col-xs-6">
-                <div class="form-group">
-                  {!! Form::label('firstname', 'First name:') !!}
-                  {!! Form::text('firstname', null, ['class' => 'form-control', 'required' => 'required', 'autocomplete' => 'firstname']) !!}
+                <div class='form-field'> 
+                  <div class="form-group">
+                    {!! Form::label('firstname', 'First name:') !!}
+                    {!! Form::text('firstname', null, ['class' => 'form-control', 'required' => 'required', 'autocomplete' => 'firstname']) !!}
+                  </div>
                 </div>
               </div>
               <div class="col-xs-6">
-                <div class="form-group">
-                  {!! Form::label('lastname', 'Last name:') !!}
-                  {!! Form::text('lastname', null, ['class' => 'form-control', 'required' => 'required', 'autocomplete' => 'lastname']) !!} 
+                <div class='form-field'> 
+                  <div class="form-group">
+                    {!! Form::label('lastname', 'Last name:') !!}
+                    {!! Form::text('lastname', null, ['class' => 'form-control', 'required' => 'required', 'autocomplete' => 'lastname']) !!} 
+                  </div>
                 </div>
               </div>  
             </div>              
@@ -130,37 +183,35 @@
               
             <div class="row">
               <div class="col-xs-2">
-                {!! Form::label('duedate', 'Due date:') !!}
-              </div>
-              <div class="col-xs-3">
-                <div class="form-group">
-                  {!! Form::label('duemonth', 'Month:') !!}
-                  {!! Form::text('duemonth', null, ['class' => 'form-control duemonth', 'maxlength' => 2, 'required' => 'required', 'placeholder' =>'••', 'autocomplete' => 'duemonth']) !!}
+                <div class='form-field'> 
+                 <div class="form-group">
+                    {!! Form::label('duedate', 'Due date:') !!}
+                    {!! Form::text('duedate', null, ['class' => 'form-control duedate', 'required' => 'required', 'placeholder' =>'••/••••', 'autocomplete' => 'dueyear']) !!}
+                  </div>
                 </div>
               </div>
               <div class="col-xs-3">
-                <div class="form-group">
-                  {!! Form::label('dueyear', 'Year:') !!}
-                  {!! Form::text('dueyear', null, ['class' => 'form-control dueyear', 'maxlength' => 4, 'required' => 'required', 'placeholder' =>'••••', 'autocomplete' => 'dueyear']) !!}
-                </div>
-              </div>
-              <div class="col-xs-3">
-                <div class="form-group">
-                  {!! Form::label('securitycode', 'Security code:') !!}
-                  {!! Form::text('securitycode', null, ['class' => 'form-control securitycode', 'required' => 'required', 'placeholder' =>'•••', 'autocomplete' => 'off']) !!}
+                <div class='form-field'> 
+                  <div class="form-group">
+                    {!! Form::label('securitycode', 'Security code:') !!}
+                    {!! Form::text('securitycode', null, ['class' => 'form-control securitycode', 'required' => 'required', 'placeholder' =>'•••', 'autocomplete' => 'off']) !!}
+                  </div>
                 </div>
               </div>
 
             </div>              
               
 
-              <div class="form-group">
-                  {!! Form::submit('Pay!') !!}
+              <div class="form-group center">
+
+                  {!! Form::submit('Pay for your food!', ['class' => 'btn btn-default']) !!}
            
               </div>
 
-              <h2 class="validation"></h2>
-
+              <div class="alert alert-danger validation" role="alert" hidden="false">
+                  <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+              </div>
+              
 
           {!! Form::close() !!}
         </div>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,35 +3,94 @@
 
     <title>TPAGA</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  {!! Html::style('assets/css/bootstrap.css') !!}
+ 
+  <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+ 
+  <script src="{{asset('assets/js/bootstrap.js')}}"></script>
+  <script type="text/javascript">
+    $('#tapagacarousel').carousel({
+        interval: 1200
+    });
+  </script>
+  
     @section('header')
       
     @show
 
+  <style type="text/css">
+     body { background: #D3D3D3 !important; } 
+  </style>
+
   </head>
   <body>
-
-
     <div class="container">
       <div class="page-header">
-        <h1>COMBOMARKET<small>Eat online!</small></h1>
+        <div class="row">
+          <div class="col-xs-8">
+            <h1>COMBOMARKET<small>Eat online!</small></h1>
+          </div>
+          <div class="col-xs-4">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Search for...">
+              <span class="input-group-btn">
+                <button class="btn btn-default" type="button">Go!</button>
+              </span> 
+            </div>
+          </div>
+        </div>
       </div>
     </div>
 
+
+
+    <div class="container">
+      <div id="tpagacarousel" class="carousel slide">
+        <ol class = "carousel-indicators">
+            <li data-target = "#tpagacarousel" data-slide-to = "0" class="active"></li>
+            <li data-target = "#tpagacarousel" data-slide-to = "1"></li>
+        </ol>
+
+        <div class ="carousel-inner">
+          <div class = "item active">
+            <img src = "{{asset('assets/images/bannerimg6.jpg')}}" alt ="bimg1" class="img-responsive">
+          </div>
+
+          <div class = "item">
+            <img src = "{{asset('assets/images/bannerimg7.jpg')}}" alt ="bimg1" class="img-responsive">
+          </div>
+        </div>
+
+        <a class="carousel-control left" href ="#tpagacarousel" data-slide ="prev">
+          <span class = "icon-prev"></span>
+        </a>
+
+        <a class="carousel-control right" href ="#tpagacarousel" data-slide ="next">
+          <span class = "icon-next"></span>
+        </a>
+
+      </div>
+    </div>
+
+
+
+    @section('maincontent')
+      
+    @show
     <div class="container">
       <div class="row">
         <h1> <img src="{{asset('assets/images/logo.png')}}" alt="TPAGA" height="90" width="250"> </h1>
       </div>
     </div>
 
-        @section('content')
-          
-        @show
+
+    @section('paymentcontent')
+      
+    @show
 
     
-  </body>
+ </body>
+
 </html>
